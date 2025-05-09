f1bb280f2de9a8d08507a3c90e7242f449b4e86c
Amadou Lamine KEBE <amadoulamine1@gmail.com>
2025-05-09 12:44:26 +0000

    turbo frame

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,13 @@
-import './bootstrap.js';
+import * as Turbo from "@hotwired/turbo";
+import "./bootstrap.js";
 /*
  * Welcome to your app's main JavaScript file!
  *
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/app.css';
-import alienGreeting from './lib/alien-greeting.js';
+import "./styles/app.css";
+import alienGreeting from "./lib/alien-greeting.js";
 /*import JSConfetti from 'js-confetti';
 
 const jsConfetti = new JSConfetti();
@@ -16,7 +17,6 @@ jsConfetti.addConfetti({
   confettiNumber: 100,
 });
 */
-alienGreeting('Give us all your candy!', false);
+alienGreeting("Give us all your candy!", false);
 
-
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+console.log("This log comes from assets/app.js - welcome to AssetMapper! 🎉");
