aa61401d9a39cca95f46369fbd49c483ddc73b1d
Amadou Lamine KEBE <amadoulamine1@gmail.com>
2025-05-12 12:54:30 +0000

    sorting

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,13 +2,15 @@
 
 namespace App\Controller;
 
+use Pagerfanta\Pagerfanta;
 use App\Repository\PlanetRepository;
 use App\Repository\VoyageRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Pagerfanta\Doctrine\ORM\QueryAdapter;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MainController extends AbstractController
 {
@@ -16,16 +18,28 @@ class MainController extends AbstractController
     public function homepage(
         VoyageRepository $voyageRepository,
         PlanetRepository $planetRepository,
+        #[MapQueryParameter] int $page = 1,
         #[MapQueryParameter('query')] string $query = null,
         #[MapQueryParameter('planets', \FILTER_VALIDATE_INT)] array $searchPlanets = [],
+        #[MapQueryParameter] string $sort = 'leaveAt',
+        #[MapQueryParameter] string $sortDirection = 'ASC',
     ): Response
     {
+        $validSorts = ['purpose', 'leaveAt','planet'];
+        $sort = in_array($sort, $validSorts) ? $sort : 'leaveAt';
+        $pager = Pagerfanta::createForCurrentPageWithMaxPerPage(
+            new QueryAdapter($voyageRepository->findBySearchQueryBuilder($query, $searchPlanets, $sort, $sortDirection)),
+            $page,
+            10
+        );
         $voyages = $voyageRepository->findBySearch($query, $searchPlanets);
 
         return $this->render('main/homepage.html.twig', [
-            'voyages' => $voyages,
+            'voyages' => $pager,
             'planets' => $planetRepository->findAll(),
             'searchPlanets' => $searchPlanets,
+            'sort' => $sort,
+            'sortDirection' => $sortDirection,
         ]);
     }
 }
